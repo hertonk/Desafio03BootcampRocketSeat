592df688941c22cf3085db0489088d6d6a41769f
Herton F. Vilarim <hertonk@gmail.com>
2020-04-14 19:33:22 -0300

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Desafio03BootcampRocketSeat
-Resolução do Desafio 3 do Bootcamp da Rocketseat.
+# Resolução do Desafio 03 - GoStack
+
+<h3 align="center">
+  Desafio 03: Conceitos do ReactJS
+</h3>
+
+## Descrição
+
+Resolução do Desafio 03 do Bootcamp da RocketSeat.
+
+### Testando a resolução
+
+Para utilizar a resolução do desafio, você deve baixar este repositório rodar o comando `yarn` para a instalação das dependências e para "rodar" o front-end deve utilizar o comando `yarn start`.
+
+Abaixo segue uma descrição das funcionalidades:
+
+- **`listar repositórios`**: Ao acessar o front-end é exibida uma lista com todos os respositórios cadastrados no back-end da aplicação.
+
+- **`remover repositório`**: Ao clicar em "Remover" o repositório será removido da aplicação.
+
+- **`adicionar repositório`**: Ao clicar no botão "Adicionar" será adicionado um novo repositório na aplicação.
+
+---
+
+Feito por Herton F. Vilarim.
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    
+    const response = await api.post('repositories', {
+      title: `Projeto X ${Date.now()}`,
+      url: `http://www.${Date.now()}.com`,
+      techs: ["Node.js", "ReactJS"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
+  }
+
+  async function handleRemoveRepository(id) {
+    api.delete(`repositories/${id}`).then(response => {
+      api.get('repositories').then(response => {
+        setRepositories(response.data);
+      });
+    });
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+      {repositories.map(repository => ( 
+        <li>
+        
+        {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+      ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
